cc86496e1ea4b5baa0b678c2c1a454020f58ab7b
Bondhon1 <152187754+Bondhon1@users.noreply.github.com>
2025-04-28 22:25:31 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sql12775784:2XV7IU1ZKY@sql12.freesqldatabase.com:3306/sql12775784'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SECRET_KEY = 'your_secret_key'
+
